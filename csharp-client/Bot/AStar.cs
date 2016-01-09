5e9554e12dcc360082e2eb998daaedbaf0a5ddb9
Denis Shelgunov <dshelgunov@coveo.com>
2016-01-09 09:19:27 -0500

    astart

---
+++ csharp-client/Bot/AStar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoveoBlitz;
+
+namespace Coveo.Bot
+{
+    public class AStar: IPathfinder
+    {
+        public class InternalTile
+        {
+            public Pos TilePos;
+            public int Weight;
+        }
+
+        public PathData pathTo(Pos target, Pos currentLocation, Tile[][] board, int spikeCost = 5)
+        {
+            HashSet<Pos>
+        }
+    }
+}
