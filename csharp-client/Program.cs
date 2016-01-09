beb4ccac4f6ee3187813359a815fa372a07707bf
clechasseur <devnull@localhost>
2016-01-09 10:18:34 -0500

    + HAX

--- csharp-client/Program.cs
+++ csharp-client/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) 2005-2016, Coveo Solutions Inc.
 
+using System;
 using CoveoBlitz;
 using CoveoBlitz.RandomBot;
-using System;
 using CoveoBlitz.WowBlitzAwsome.Bot;
 
 namespace Coveo
@@ -19,27 +19,30 @@ namespace Coveo
         private static void Main(string[] args)
         {
             if (args.Length < 2) {
-                Console.WriteLine("Usage: myBot.exe key training|arena gameId");
+                Console.WriteLine("Usage: myBot.exe key training|arena|HAX gameId");
                 Console.WriteLine("gameId is optionnal when in training mode");
                 Console.ReadKey();
                 return;
             }
 
             string serverURL = "http://blitz2016.xyz:8080";
+            bool trainingMode = args[1] == "training" || args[1] == "HAX";
             string gameId = args.Length == 3 ? args[2] : null;
 
             IBestChoice bestChoice = new EvenBestChoice();
             IPathfinder pathfinder = null;
 
             ISimpleBot bot;
-            if (bestChoice != null && pathfinder != null) {
+            if (args[1] == "HAX") {
+                bot = new ManualBot();
+            } else if (bestChoice != null && pathfinder != null) {
                 bot = new BestBot(bestChoice, pathfinder);
             } else {
                 bot = new RandomBot();
             }
-
+            
             SimpleBotRunner runner = new SimpleBotRunner(
-                new ApiToolkit(serverURL, args[0], args[1] == "training", gameId),
+                new ApiToolkit(serverURL, args[0], trainingMode, gameId),
                 bot);
 
             runner.Run();
