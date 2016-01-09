9669485e126831e57adf6c8a55d7ef1a98fac79a
clechasseur <devnull@localhost>
2016-01-09 09:25:55 -0500

    + IBestChoice

---
+++ csharp-client/IBestChoice.cs
@@ -0,0 +1,13 @@
+﻿using CoveoBlitz;
+
+namespace Coveo
+{
+    /// <summary>
+    /// Interface for a module that can provide the "best choice" for the next move.
+    /// Must return a value from the <see cref="CoveoBlitz.Direction"/> class.
+    /// </summary>
+    public interface IBestChoice
+    {
+        string BestMove(GameState gameState, IPathfinder pathfinder);
+    }
+}
